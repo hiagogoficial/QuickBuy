8a3d950d065de180119f0e39d1e5efa81adf356c
Hiago G. Paixão <goncalveshiago@outlook.com>
2020-06-15 17:46:17 -0300

    Construção dos Repositorios

---
+++ QuickBuy.Dominio/Contratos/IBaseReporitorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace QuickBuy.Dominio.Contratos
+{
+    public interface IBaseReporitorio<TEntity> : IDisposable where TEntity : class
+    {
+        void Adcionar(TEntity entity);
+
+        TEntity ObterPorID(int id);
+
+        IEnumerable<TEntity> ObterTodos();
+
+        void Atualizar(TEntity entity);
+
+        void Remover(TEntity entity);
+    }
+}
---
+++ QuickBuy.Repositorio/Repositorios/BaseRepositorio.cs
@@ -0,0 +1,38 @@
+﻿using QuickBuy.Dominio.Contratos;
+using System.Collections.Generic;
+
+namespace QuickBuy.Repositorio.Repositorios
+{
+    public class BaseRepositorio<TEntity> : IBaseReporitorio<TEntity> where TEntity : class
+    {
+        public void Adcionar(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Atualizar(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TEntity ObterPorID(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> ObterTodos()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remover(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

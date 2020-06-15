6c4e5c40ef96663b70e8793a4a266187e28d5941
Hiago G. Paixão <goncalveshiago@outlook.com>
2020-06-15 16:15:35 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,37 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        public DateTime DataPedido { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public DateTime DataPrevisaoEntrega { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string EndereçoCompleto { get; set; }
+
+        public int NumeroEndereço { get; set; }
+
+        public int FormaPagamentoID { get; set; }
+
+        public FormaPagamento FormaPagamento { get; set; }
+
+        /// <summary>
+        /// Pedido deve ter pelo menos um Itém de Pedido
+        /// ou Muitos Itens de Pedido
+        /// </summary>
+        public ICollection <ItemPedido> ItensPedidos { get; set; }
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Nome { get; set; }
+        
+        public string Sobrenome { get; set; }
+
+        /// <summary>
+        /// Um usuário pode ter nenhum ou muitos pedidos
+        /// </summary>
+        
+        public ICollection <Pedido> Pedidos { get; set; }
+    }
+}
